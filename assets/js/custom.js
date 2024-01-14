78c8e8f82cc58f56da5dbc6994e1925b7933c804
Bogdan08st <bogdan08st@gmail.com>
2024-01-14 19:11:03 +0100

    feat: choice

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,50 +36,7 @@ const InstallEventAction = function(){
 document.querySelector('.page-action') ? InstallEventAction() : null;
 
 const InstallStatisticCompany = () =>{
-    const StatisticsCompany = document.querySelectorAll('.statistics-company')
-    const RenderSiteStatistics = (info)=>{
-        let list = info.map((element) =>{
-            return `
-                <div id="1" class="row info-statistics_container">
-                    <div class="col-4 info-statistics">
-                        <div class="row">
-                            <div class="img-container">
-                                <img src="assets/img/choice-statistics_icon.svg" alt="icon">
-                            </div>
-                            <div class="col col-content">
-                                <div class="statistic-title">${element.statTitle}</div>
-                                <div class="statistic-subtitle">${element.subTitle}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="2" class="col-4 info-statistics">
-                        <div class="row">
-                            <div class="img-container">
-                                <img src="assets/img/choice-person_icon.svg" alt="icon">
-                            </div>
-                            <div class="col col-content">
-                                <div class="statistic-title">10M+</div>
-                                <div class="statistic-subtitle">Trusted Wallets Investor</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="3" class="col-4 info-statistics">
-                        <div class="row">
-                            <div class="img-container">
-                                <img src="assets/img/choice-earth_icon.svg" alt="icon">
-                            </div>
-                            <div class="col col-content">
-                                <div class="statistic-title">195</div>
-                                <div class="statistic-subtitle">Countries Supported</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `;
-        });
-        StatisticsCompany.innerHTML = list;
-    }
-    RenderSiteStatistics(SiteStatistics);
+    const StatisticsCompany = document.querySelector('.statistics-company')
 }
 
 document.querySelector('.statistics-company') ? InstallStatisticCompany() : null;
